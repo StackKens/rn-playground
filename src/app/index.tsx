eea8d05292dc6514f8a865cc9af5d873a5c0ec47
StackKens <ofwonoalex256@gmail.com>
2026-08-01 13:12:57 +0300

    feat: add input width 64, button 32 for better UI visibility

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,9 +6,9 @@ export default function HomeScreen() {
   const [visible, setVisible] = useState(false);
 
   return (
-    <SafeAreaView className="flex-1 bg-slate-950">
+    <SafeAreaView className="flex-1 bg-slate-50">
       <View className="flex-1 items-center justify-center gap-2">
-        <Text className="text-3xl font-bold text-white">RN Playground</Text>
+        <Text className="text-3xl font-bold text-slate-800">RN Playground</Text>
         <Text className="text-base text-slate-400">
           Practice React Native concepts here.
         </Text>
@@ -18,12 +18,12 @@ export default function HomeScreen() {
             placeholder="Type..."
             value={text}
             onChangeText={setText}
-            className="px-3 py-2 text-white border border-gray-500 rounded-full w-70"
+            className="px-3 py-2 text-slate-950 border border-gray-500 rounded-full w-64"
           />
         </View>
 
         <Pressable
-          className="rounded-3xl w-20 py-3 px-3 border border-gray-500"
+          className="rounded-3xl w-32 py-3 px-3 border border-gray-500"
           onPress={() => setVisible((prev) => !prev)}
         >
           <Text className="text-blue-600">Show text</Text>
@@ -31,7 +31,7 @@ export default function HomeScreen() {
 
         {visible && (
           <View>
-            <Text className="text-white font-bold text-2xl py-2">
+            <Text className="text-slate-950 font-bold text-2xl py-2">
               {text ? `You have typed: ${text}` : "Please type something"}
             </Text>
           </View>
