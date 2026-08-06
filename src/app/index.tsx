623e04ac114ba656dfb14f28cd7e8a63c263fbb9
StackKens <ofwonoalex256@gmail.com>
2026-08-06 11:14:33 +0300

    feat: practice state amangement, functional state update

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,6 +5,8 @@ export default function HomeScreen() {
   const [text, setText] = useState("");
   const [visible, setVisible] = useState(false);
 
+  const [sum, setSum] = useState(0);
+
   return (
     <SafeAreaView className="flex-1 bg-slate-50">
       <View className="flex-1 items-center justify-center gap-2">
@@ -18,7 +20,7 @@ export default function HomeScreen() {
             placeholder="Type..."
             value={text}
             onChangeText={setText}
-            className="px-3 py-2 text-slate-950 border border-gray-500 rounded-full w-64"
+            className="px-3 py-4 text-slate-950 border border-gray-500 rounded-full w-64"
           />
         </View>
 
@@ -26,7 +28,7 @@ export default function HomeScreen() {
           className="rounded-3xl w-32 py-3 px-3 border border-gray-500"
           onPress={() => setVisible((prev) => !prev)}
         >
-          <Text className="text-blue-600">Show text</Text>
+          <Text className="text-blue-600 text-center">Show text</Text>
         </Pressable>
 
         {visible && (
@@ -36,6 +38,29 @@ export default function HomeScreen() {
             </Text>
           </View>
         )}
+      </View>
+
+      <View className="py-4 flex-1">
+        <View className="flex-row gap-2 ml-8">
+          <Pressable
+            className="rounded-3xl w-32 py-3 px-3 border border-gray-500"
+            onPress={() => setSum((prev) => prev + 1)}
+          >
+            <Text className="text-center">Add</Text>
+          </Pressable>
+          <Pressable
+            className="rounded-3xl w-32 py-3 px-3 border border-gray-500"
+            onPress={() => setSum(0)}
+          >
+            <Text className="text-center text-red-300 font-bold">Reset</Text>
+          </Pressable>
+        </View>
+
+        <View className="py-4">
+          <Text className="text-black font-sm text-2xl  py-4 text-center bg-gray-950 text-white">
+            {sum}
+          </Text>
+        </View>
       </View>
     </SafeAreaView>
   );
